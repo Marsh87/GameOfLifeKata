073000e1e8198ca86fd44a6637e93621be292d9d
Mahesh Moodley <Mahesh.Moodley@chillisoft.co.za>
2021-01-22 16:01:47 +0200

    Add project

---
+++ GameOfLife.cs
@@ -0,0 +1,87 @@
+﻿namespace GameOfLifeKata
+{
+    public class GameOfLife
+    {
+        public int[,] Execute(int[,] gamestate)
+        {
+            for (int row = 0; row < gamestate.GetUpperBound(0); row++)
+            {
+                for (int column = 0; column < gamestate.GetUpperBound(1); column++)
+                {
+                    var cell = gamestate[row, column];
+                    if (cell == 1)
+                    {
+                        var sumOfNeighbours = CalculateNeighboursState(gamestate, column, row);
+                        if (sumOfNeighbours < 2)
+                        {
+                            gamestate[row, column] = 0;
+                        }
+                        if (sumOfNeighbours  ==2 || sumOfNeighbours ==3)
+                        {
+                            gamestate[row, column] = 0;
+                        }
+                        if (sumOfNeighbours > 3)
+                        {
+                            gamestate[row, column] = 0;
+                        }
+                    }
+                    else
+                    {
+                        var sumOfNeighbours = CalculateNeighboursState(gamestate, column, row);
+                        if (sumOfNeighbours == 3)
+                        {
+                            gamestate[row, column] = 1;
+                        }
+                    }
+                }
+            }
+            return gamestate;
+        }
+
+        private static int CalculateNeighboursState(int[,] gamestate, int column, int row)
+        {
+            var sumOfNeighbours = 0;
+            if (column > 0)
+            {
+                sumOfNeighbours += gamestate[row, column - 1];
+            }
+
+            if (gamestate.GetUpperBound(1) > 0)
+            {
+                sumOfNeighbours += gamestate[row, column + 1];
+            }
+
+            if (row > 0)
+            {
+                sumOfNeighbours += gamestate[row - 1, column];
+            }
+
+            if (gamestate.GetUpperBound(0) > 0)
+            {
+                sumOfNeighbours += gamestate[row + 1, column];
+            }
+
+            if (row > 0 && gamestate.GetUpperBound(1) > 0)
+            {
+                sumOfNeighbours += gamestate[row - 1, column + 1];
+            }
+
+            if (gamestate.GetUpperBound(0) > 0 && gamestate.GetUpperBound(1) > 0)
+            {
+                sumOfNeighbours += gamestate[row + 1, column + 1];
+            }
+
+            if (gamestate.GetUpperBound(0) > 0 && column > 0)
+            {
+                sumOfNeighbours += gamestate[row + 1, column - 1];
+            }
+
+            if (row > 0 && column > 0)
+            {
+                sumOfNeighbours += gamestate[row - 1, column - 1];
+            }
+
+            return sumOfNeighbours;
+        }
+    }
+}
---
+++ GameOfLifeKata.Tests/GameOfLifeTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace GameOfLifeKata.Tests
+{
+    [TestFixture]
+    public class GameOfLifeTests
+    {
+        // We could of taken smaller steps based of the instructions
+        [Test]
+        public void Execute_GivenInitialGameStateIsBlockShouldReturnBlock()
+        {
+            int[,] initialGameState = new int[, ]{{0,0,0,0},{0,1,1,0},{0,1,1,0},{0,0,0,0}};
+            var sut = new GameOfLife();
+            var result =sut.Execute(initialGameState);
+            var expected = new int[,]
+                {{0, 0, 0, 0, 0}, {0, 0, 1, 0, 0}, {0, 1, 0, 1, 0}, {0, 1, 0, 1, 0}, {0, 0, 1, 0, 0}, {0, 0, 0, 0, 0}};
+            Assert.AreEqual(initialGameState,result);
+        }
+        
+        [Test]
+        public void Execute_GivenInitialGameStateIsBeehiveShouldReturnBeehive()
+        {
+            int[,] initialGameState = new int[,]
+                {{0, 0, 0, 0, 0}, {0, 0, 1, 0, 0}, {0, 1, 0, 1, 0}, {0, 1, 0, 1, 0}, {0, 0, 1, 0, 0}, {0, 0, 0, 0, 0}};;
+            var sut = new GameOfLife();
+            var result =sut.Execute(initialGameState);
+            Assert.AreEqual(initialGameState,result);
+        }
+    }
+}
