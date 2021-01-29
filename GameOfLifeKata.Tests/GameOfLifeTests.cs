6348c598cf040bfbec1a52062cee6aecaab419bb
Mahesh Moodley <Mahesh.Moodley@chillisoft.co.za>
2021-01-29 09:28:45 +0200

    Add missing tests

--- GameOfLifeKata.Tests/GameOfLifeTests.cs
+++ GameOfLifeKata.Tests/GameOfLifeTests.cs
@@ -27,5 +27,53 @@ namespace GameOfLifeKata.Tests
             var result =sut.Execute(initialGameState);
             Assert.AreEqual(initialGameState,result);
         }
+        
+        [Test]
+        public void Execute_GivenInitialGameStateHasLiveCellWithFewerThanTwoNeighbours_ThenCellShouldDie()
+        {
+            int[,]  initialGameState = new int[, ]{{0,0,0,0},{0,1,1,0},{0,0,0,0},{0,0,0,0}};
+            var sut = new GameOfLife();
+            var result =sut.Execute(initialGameState);
+            var expected  = new int[, ]{{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}};
+            Assert.AreEqual(expected,result);
+        }
+        
+        [Test]
+        public void Execute_GivenInitialGameStateHasLiveCellWithTwoNeighbours_ThenCellShouldLive()
+        {
+            int[,]  initialGameState = new int[, ]{{0,0,0,0},{0,1,1,0},{0,1,0,0},{0,0,0,0}};
+            var sut = new GameOfLife();
+            var result =sut.Execute(initialGameState);
+            Assert.AreEqual(initialGameState,result);
+        }
+        
+        [Test]
+        public void Execute_GivenInitialGameStateHasLiveCellWithThreeNeighbours_ThenCellShouldLive()
+        {
+            int[,]  initialGameState = new int[, ]{{0,0,0,0},{0,1,1,0},{0,1,1,0},{0,0,0,0}};
+            var sut = new GameOfLife();
+            var result =sut.Execute(initialGameState);
+            Assert.AreEqual(initialGameState,result);
+        }
+        
+        [Test]
+        public void Execute_GivenInitialGameStateHasLiveCellWithMoreThanThreeNeighbours_ThenCellShouldDie()
+        {
+            int[,]  initialGameState = new int[, ]{{0,0,0,0},{1,1,1,0},{0,1,0,0},{0,0,0,0}};
+            var sut = new GameOfLife();
+            var result =sut.Execute(initialGameState);
+            var expected = new int[, ]{{0,0,0,0},{1,0,1,0},{0,1,0,0},{0,0,0,0}};
+            Assert.AreEqual(initialGameState,result);
+        }
+        
+        [Test]
+        public void Execute_GivenInitialGameStateHasDeadCellWithMoreThanThreeNeighbours_ThenCellShouldBecomeLive()
+        {
+            int[,]  initialGameState = new int[, ]{{0,0,0,0},{1,0,1,0},{0,1,0,0},{0,0,0,0}};
+            var sut = new GameOfLife();
+            var result =sut.Execute(initialGameState);
+            var expected = new int[, ]{{0,0,0,0},{0,1,0,0},{0,0,0,0},{0,0,0,0}};
+            Assert.AreEqual(initialGameState,result);
+        }
     }
 }
